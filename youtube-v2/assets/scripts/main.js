0a69b3e8ceef962af5f849487ffae7bdc391e5c2
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-02-16 15:01:15 +0530

    updated

--- youtube-v2/assets/scripts/main.js
+++ youtube-v2/assets/scripts/main.js
@@ -1,3 +1,4 @@
+/* the following is for js-demo.html page */
 function handleStylize() {
   // DOM querying and manipulation
   const myDiv = document.getElementById("myDiv1");
@@ -17,10 +18,55 @@ function handleAddElement() {
   // create the li element <li></li>
   const li = document.createElement("li"); // <li></li>
   // create the text content that should later be part of the li 'Home'
-  const text = document.createTextNode("Home"); // Home
+  const text = document.createTextNode(document.getElementById("menuInput").value); 
   // put the text inside the li   <li>Home</li>
   li.appendChild(text); // <li>Home</li>
 
   // append the above li in parent element
   menuList.appendChild(li);
 }
+
+const skills = [
+  {
+    id: 1,
+    topic: "HTML",
+  },
+  {
+    id: 2,
+    topic: "css",
+  },
+  {
+    topic: 3,
+    topic: "js",
+  }
+];
+
+function handleAddSkill() {
+  // TODO: implement the array map and use template literal to create multiple elements
+  // then insert it in elemetn with id #skillList
+}
+
+
+// The following is for index.html page 
+const videos = [
+  {
+    id: 52345235,
+    title: "Football Highlights of 2022",
+    videoThumbnailUrl:
+      "https://via.placeholder.com/240x180/09f/fff?text=Football Highlights",
+    channelName: "Channel B",
+    channelThumbnailUrl: "https://via.placeholder.com/32/f00/fff?text=B",
+    views: "1M",
+    publishedDate: "2 weeks ago",
+  },
+  {
+    id: 89679431,
+    title: "Cricket Highlights of 2023",
+    videoThumbnailUrl:
+      "https://via.placeholder.com/240x180/09f/fff?text=Cricket Highlights",
+    channelName: "Channel B",
+    channelThumbnailUrl: "https://via.placeholder.com/32/f00/fff?text=Z",
+    views: "2M",
+    publishedDate: "5 weeks ago",
+  },
+];
