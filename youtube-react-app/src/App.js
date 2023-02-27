4d1e7f4e043b84511347b4e48ea0cb78a1cc8830
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-02-27 15:03:57 +0530

    updated

---
+++ youtube-react-app/src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header/Header';
+
+// importing css
+import './App.css';
+
+// Component 
+function App() {
+  // must return JSX
+  return (
+    // Here comes JSX
+    // Ideal place for you to build the layout
+    <div>
+      <Header />
+      <h1>Success!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ youtube-react-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+// Fn component with Named Fn
+// comp can have js, jsx and css (optional)
+function Header() {
+  // must return JSX 
+  return(
+    <div>
+      my header comes here
+    </div>
+  )
+}
+
+export default Header;
