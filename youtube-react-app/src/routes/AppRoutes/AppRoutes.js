136cc2c3861d02228ce6629b444b7b65a936c104
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-04-07 11:45:27 +0530

    updated

--- youtube-react-app/src/routes/AppRoutes/AppRoutes.js
+++ youtube-react-app/src/routes/AppRoutes/AppRoutes.js
@@ -1,25 +1,32 @@
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AboutUsPage from '../../pages/AboutUsPage/AboutUsPage';
-import ContactUsPage from '../../pages/ContactUsPage/ContactUsPage';
-import HocDemoPage from '../../pages/HocDemoPage/HocDemoPage';
-import HomePage from '../../pages/HomePage/HomePage';
-import HooksDemoPage from '../../pages/HooksDemoPage/HooksDemoPage';
-import ShopPage from '../../pages/ShopPage/ShopPage';
-import VideoPage from '../../pages/VideoPage/VideoPage';
-import UnitTestingDemoPage from '../../pages/UnitTestingDemoPage/UnitTestingDemoPage';
 
+import HomePage from '../../pages/HomePage/HomePage'; // not lazy loaded
+import AboutUsPage from '../../pages/AboutUsPage/AboutUsPage'; // not lazy loaded
+
+// the following components are lazy loaded
+const ContactUsPage = React.lazy(() => import('../../pages/ContactUsPage/ContactUsPage'));
+const HocDemoPage = React.lazy(() => import('../../pages/HocDemoPage/HocDemoPage'));
+const VideoPage = React.lazy(() => import('../../pages/VideoPage/VideoPage'));
+const UnitTestingDemoPage = React.lazy(() => import('../../pages/UnitTestingDemoPage/UnitTestingDemoPage'));
+const HooksDemoPage = React.lazy(() => import('../../pages/HooksDemoPage/HooksDemoPage'));
+const ShopPage = React.lazy(() => import('../../pages/ShopPage/ShopPage'));
+
+// re-organize the routes for lazy loading with suspense UI
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/video' element={<VideoPage />} />
-      <Route path='/hoc-demo' element={<HocDemoPage />} />
-      <Route path='/about-us' element={<AboutUsPage />} />
-      <Route path='/contact-us' element={<ContactUsPage />} />
-      <Route path='/hooks-demo' element={<HooksDemoPage />} />
-      <Route path='/shop' element={<ShopPage />} />
-      <Route path='/unit-testing-demo' element={<UnitTestingDemoPage />} />
-    </Routes>
+    <Suspense fallback={<div className='spinner-border text-primary'></div>}>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/video' element={<VideoPage />} />
+        <Route path='/hoc-demo' element={<HocDemoPage />} />
+        <Route path='/about-us' element={<AboutUsPage />} />
+        <Route path='/contact-us' element={<ContactUsPage />} />
+        <Route path='/hooks-demo' element={<HooksDemoPage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/unit-testing-demo' element={<UnitTestingDemoPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 
